2383a909f01646b908397dad37a12c3b9532c7a6
Andrew Shannon <drewan97@gmail.com>
2024-10-28 16:05:55 +0000

    Added ground truth generation scripts

---
+++ Parameter_Error_Task/Generate_Ground_Truth_Kuramoto.jl
@@ -0,0 +1,62 @@
+using Pkg; Pkg.activate(".")
+include("$(pwd())/src/HybridRCforNLONS.jl")
+import .HybridRCforNLONS: cartesian_kuramoto, cartesian_kuramoto_ic, generate_ground_truth_data, generate_arrow
+using OrdinaryDiffEq, Random, CSV, Arrow, DataFrames
+
+#read in command line arguments
+#idx=1
+idx=parse(Int64,ARGS[1]) #read in index defining the ground truth model version: 1=Model2=asynchronous, 2=Model7=multi-frequency, 3=Model16=synchronous
+#output_path="./"
+output_path=ARGS[2] #read in absolute path to store results in.
+
+N=5 #number of oscillators
+tspan=(0.0,6200.0) #one large trajectory for each model will be generated. To be split into training, warmup and test segments as required.
+dt=0.1 #time step.
+models=[["2","2_1","2_2"],["7","7_1","7_2"],["16","16_1","16_2"]] #three instantiations of each model type.
+model_names=["asynchronous","multi-frequency","synchronous"]
+i=[2,7,16][idx] #the three model types.
+for j in 1:3
+    model=models[idx][j]
+    println("model=",model," running ODE sim")
+    rng=MersenneTwister(1234+i+j-1)
+    println("rng: ",rng)
+
+    #set parameters.
+    base_params=ones(Float64,6)
+    if i == 2 # - Model 2: Nat Freqs within [-1,+1], K=1.0 (asynch)
+        base_params[1:5]=2.0.*rand(rng,5).-1.0
+        base_params[6]=1.0 #set K.
+    elseif i == 7 # - Model 7: 4 Nat Freqs within [-1,+1], with outlier >3Hz.  K=2.0, (phase locked with high frequency)
+        base_params[1:4]=2.0.*rand(rng,4).-1.0
+        x=rand()-0.5
+        base_params[5]=(rand(rng).+3.0)*sign(x)
+        base_params[6]=2.0 #set K.
+    elseif i == 16 # - Model 16: 4 Nat Freqs within [-1,+1] K=4.0 (synchronised)
+        base_params[1:5]=2.0.*rand(rng,5).-1.0
+        base_params[6]=4.0 #set K.
+    end
+    println("params: ",base_params)
+    #generate ground truth data
+    #cartesian_kuramoto is in phase-component space. so with N=5, the state vector is 10-dimensional. 
+    #arranged such that 1:5 are the x components and 6:10 are the y components.
+    ic=cartesian_kuramoto_ic(N) #same initial conditions for every run (internally this uses a MersenneTwister rng with seed 1234)
+    gt_data=generate_ground_truth_data(cartesian_kuramoto,ic,base_params,tspan,1e7,dt)
+    gt_data=permutedims(reduce(hcat,gt_data.u))
+
+    #write to csv and compress as arrow file.
+    name="Model_$(model_names[idx])_$(j)_ground_truth_data"
+    println("writing csv")
+    CSV.write(output_path*name*".csv",DataFrame(gt_data,:auto),writeheader=true)
+    println("writing arrow")
+    generate_arrow(name,output_path)
+    println("deleting csv")
+    rm(output_path*name*".csv")
+end
+
+#test output: open data and plot
+# using Plots
+# j=1 #model instantiation number
+# gt_read=Matrix(DataFrame(Arrow.Table(output_path*"Model_$(model_names[idx])_$(j)_ground_truth_data.arrow.lz4")))
+# plot(gt_read[:,1:5],label="",color=:blue,ylims=(-1.0,1.0))
+# plot!(gt_read[:,6:10],label="",color=:red)
+
---
+++ Residual_Physics_Task/Generate_Ground_Truth_Biharmonic_Kuramoto.jl
@@ -0,0 +1,52 @@
+using Pkg; Pkg.activate(".")
+include("$(pwd())/src/HybridRCforNLONS.jl")
+import .HybridRCforNLONS: biharmonic_kuramoto, biharmonic_kuramoto_p, biharmonic_kuramoto_ic, reset_condition1, reset_affect1!, reset_condition2, reset_affect2!, generate_arrow
+using OrdinaryDiffEq, Random, DataFrames, CSV, Arrow
+
+#four conditions: synchronous, asynchronous, heteroclinic cycles and self consistent partial synchrony,.
+
+# Parameters:
+# a=0.2. N=10. K=1. nat freqs drawn from a very close distribution: Lorentzian(0.0, 0.1), Gamma2=pi
+# Synch: Gamma1=6.283 (2pi)
+# Asynch: Gamma1=pi
+# Heteroclinic Cycles: Gamma1=1.300
+# Self Consistent Partial Synchrony: Gamma1=1.5
+
+#these are therefore 4 models for now. with some given random seed generating the natural frequencies from a lorentzian distribution. (1234+i) with i = [1,2,3,4] for synch, asynch, HC, SCPS
+
+output_path="/Users/as15635/Documents/Projects/KnowledgeReservoirs2/test/ExtendedKuramoto/ExtKuramotoGT/"
+output_path="./"
+cases=["Synch","Asynch","HeteroclinicCycles","SelfConsistentPartialSynchrony"]
+γ_1s=[2*Float64(pi),Float64(pi),1.3,1.5]
+γ_2=Float64(pi)
+a=0.2
+N=10 #number of oscillators
+K=1.0 #coupling strength
+tspan=(0.0,6200.0) #one large trajectory for each model will be generated. To be split into training, warmup and test segments as required.
+dt=0.1 #time step.
+μ=0.0 #mean of the natural frequency distribution
+Δω=0.01 #width of the natural frequency distribution
+
+callback=CallbackSet(
+        VectorContinuousCallback(reset_condition1,reset_affect1!,N),
+        VectorContinuousCallback(reset_condition2,reset_affect2!,N)
+        )
+
+for (cidx,case) in enumerate(cases)
+    rng=MersenneTwister(1234+cidx)
+    base_params=biharmonic_kuramoto_p(rng,N,μ,Δω,K,a,γ_1s[cidx],γ_2)
+    ic=biharmonic_kuramoto_ic(N) #same initial conditions for every run (internally this uses a MersenneTwister rng with seed 1234)
+    prob=ODEProblem(biharmonic_kuramoto,ic,tspan,base_params;callback=callback)
+    gt_data=permutedims(reduce(hcat,solve(prob,Rodas4P(),dtmax=1/32,adaptive=true,saveat=dt).u))
+    name="ExtKuramoto_$(case)_ground_truth_data"
+    CSV.write(output_path*name*".csv",DataFrame(gt_data,:auto),writeheader=true)
+    generate_arrow(name,output_path)
+    rm(output_path*name*".csv")
+end
+
+#test: read and plot trajectories:
+# using Plots
+# for case in cases
+#     gt_read=Matrix(DataFrame(Arrow.Table(output_path*"ExtKuramoto_$(case)_ground_truth_data.arrow.lz4")))
+#     display(plot(gt_read[:,1:end]))
+# end
