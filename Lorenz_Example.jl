4453057b1ff552765e8bf4ebd192249ed0c0dc5e
Andrew Shannon <drewan97@gmail.com>
2024-11-06 11:02:44 +0000

    Lorenz example finished

--- Lorenz_Example.jl
+++ Lorenz_Example.jl
@@ -1,7 +1,7 @@
 using Pkg; Pkg.activate(".")
 include("$(pwd())/src/HybridRCforNLONS.jl")
 using OrdinaryDiffEq, Random, Statistics, Distributions, LinearAlgebra, Plots
-import HybridRCforNLONS: cartesian_kuramoto, cartesian_kuramoto_p, normalised_error, generate_ODE_data_task2, ESN, Hybrid_ESN, train_reservoir!, predict!, ingest_data!, initialise_reservoir!, valid_time, sqr_even_indices
+import HybridRCforNLONS: cartesian_kuramoto, cartesian_kuramoto_p, normalised_error, generate_ODE_data_task2, ESN, Hybrid_ESN, train_reservoir!, predict!, ingest_data!, initialise_reservoir!, valid_time, sqr_even_indices, valid_time
 plotlyjs()
 
 ## Generate lorenz data
@@ -25,21 +25,22 @@ sol=sol[1001:end] #remove initial transient
 train_data=reduce(hcat,sol[1:1000].u)
 target_data=reduce(hcat,sol[2:1001].u)
 warmup_data=reduce(hcat,sol[1901:2000].u)
-test_data=reduce(hcat,sol[2000:end].u)
+test_data=reduce(hcat,sol[2001:end].u)
 test_length=size(test_data,2)
+warmup_length=size(warmup_data,2)
 
 ##create standard and hybrid reservoirs
-res_size=250
+res_size=150
 mean_degree=3
 data_length=length(u0)
-spectral_radius=0.6
-input_scaling=0.25
+spectral_radius=0.4
+input_scaling=0.15
 g=1.0
 regularisation_strength=0.3
 NLAT=sqr_even_indices
 standard_reservoir=ESN(res_size, mean_degree, data_length, spectral_radius, input_scaling, g, regularisation_strength, NLAT)
 
-initialisation_rng=MersenneTwister(42)
+initialisation_rng=MersenneTwister(1234)
 initialise_reservoir!(initialisation_rng,standard_reservoir)
 
 function expert_model(du,u,p,t)
@@ -51,7 +52,7 @@ end
 
 model_dimension=3
 knowledge_ratio=0.5
-noise_distribution=Normal(0.0,0.01) #as in Pathak et al (2018)
+noise_distribution=Normal(0.0,0.05) #as in Pathak et al (2018)
 parameter_errors=(rand(initialisation_rng,noise_distribution,3))
 expert_parameters=p.*(parameter_errors.+1)
 hybrid_reservoir=Hybrid_ESN(res_size, mean_degree, data_length, model_dimension, knowledge_ratio, spectral_radius, input_scaling, g, regularisation_strength, NLAT,expert_model,expert_parameters,dt)
@@ -73,7 +74,9 @@ hybrid_prediction=predict!(hybrid_reservoir,test_length,false,false)
 
 #plot results
 xlimits=(-10.0,15)
-plot(range(start=-10+dt, stop=0,step=dt),warmup_data[1,1:end], linewidth=1, label="Warmup",color=:black,xlabel="time (s)",xlims=xlimits);
-plot!(range(start=0,stop=test_length*dt,length=test_length),test_data[1,:], linewidth=1, label="Ground Truth",color=:purple,linestyle=:dash);
+plot(size(1000,600),xlims=xlimits);
+plot!(vcat(collect(range(start=-dt*(warmup_length),stop=test_length*dt-dt,step=dt))),hcat(warmup_data,test_data)[1,:], linewidth=1, label="Ground Truth",color=:purple,linestyle=:dash)
 plot!(range(start=0,stop=test_length*dt,length=test_length),standard_prediction[1,:], linewidth=1, label="Standard",color=:blue);
 plot!(range(start=0,stop=test_length*dt,length=test_length),hybrid_prediction[1,:], linewidth=1, label="Hybrid",color=:red)
+
+
