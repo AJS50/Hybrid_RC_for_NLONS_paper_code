8df797c47bee18ff5c28de31391558858bf284e4
AJS50 <as15635@bristol.ac.uk>
2025-01-23 16:45:32 +0000

    To run all SLURM scripts

---
+++ Residual_Physics_Task/RunJobScriptsInFile.jl
@@ -0,0 +1,16 @@
+cd(@__DIR__)
+
+#collect all .sh file names
+files=readdir() 
+job_scripts=files[findall(x->occursin(".sh",x),files)]
+
+#if you want to exclude anything or filter the scripts further than just "contains .sh")
+# job_scripts=job_scripts[findall(x->occursin("OTHER_CONDITION_ON_JOB_SCRIPT_TITLES",x),job_scripts)]
+
+#run sbatch command for each.
+for job in job_scripts
+    # println(job) #use this and comment out the command if you want to just see what it will run before running it. 
+    run(Cmd(["sbatch", job]))
+    sleep(2) #may not be neccessary, but added to try and avoid clashes in precompilation
+end
+
